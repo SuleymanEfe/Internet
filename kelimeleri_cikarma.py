7fd1983885ea3c820b97ab4bd9f1132ff0e57868
SuleymanEfe <cingozilecamgoz@gmail.com>
2020-05-20 00:10:35 +0300

    v1.2

--- kelimeleri_cikarma.py
+++ kelimeleri_cikarma.py
@@ -2,7 +2,7 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup as bs
 import operator
-
+import random
 
 def sembolleri_temizle(tumkelimeler):
     sembolsuz_kelimeler = []
@@ -10,7 +10,7 @@ def sembolleri_temizle(tumkelimeler):
     for kelime in tumkelimeler:
         for sembol in semboller:
             if sembol in kelime:
-                kelime = kelime.replace(kelime, "")
+                kelime = kelime.replace(sembol, "")
         if len(kelime) > 1:
             sembolsuz_kelimeler.append(kelime)
     return sembolsuz_kelimeler
@@ -25,35 +25,38 @@ def sozluk_olustur(tumkelimeler):
             kelimesayisi[kelime] = 1
     return kelimesayisi
 
-tumkelimeler = []
-
+puanlama = {}
+Arama = input('Arama: ')
+puan = 0
 
 while True:
+
+  puan = 0
+
   try:
     with open('dosya.txt', 'r') as fin:
         data = fin.read().splitlines(True)
         url = data[0].strip()
     with open('dosya.txt', 'w') as fout:
-        fout.writelines(data[1:])
+       fout.writelines(data[1:])
   except:
-      print('DOSYANIN ICI BOS')
       break
 
   r = requests.get(url)
   soup = bs(r.content, 'html.parser')
 
-  for kelimegruplari in soup.find_all('p'):
-     icerik = kelimegruplari.text
-     kelimeler = icerik.lower().split()
+  dosya_Adi = random.randint(1, 1000000)
+  dosya = open(str(dosya_Adi) + '.txt', 'w')
 
-     for kelime in kelimeler:
-         tumkelimeler.append(kelime)
+  for kelime in soup.text.split():
+    dosya.write('{} \n'.format(kelime))
+    if Arama.lower() == kelime.lower():
+        puan += 1
+  if puan > 0:
+   puanlama[puan] = url
 
 
+for puan,url in sorted(puanlama.items()):
+    print("Puan: {}, Url: {}".format(puan,url))
 
-
-tumkelimeler = sembolleri_temizle(tumkelimeler)
-kelimesayisi = sozluk_olustur(tumkelimeler)
-for anahtar, deger in sorted(kelimesayisi.items(),key= operator.itemgetter(1)):
-    print(anahtar, ': ', deger)
 
